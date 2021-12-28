0217cc75553fe3222f1aa7bdb3cc63c1528665a8
Shaun Hume <shaunhume@Shauns-MBP.lan>
2021-12-29 01:43:27 +0800

    Added game over when going left

--- 100DaysOfCode/Program.cs
+++ 100DaysOfCode/Program.cs
@@ -1,18 +1,20 @@
 ﻿using _100DaysOfCode;
 
+var GameOver = false;
+
 Console.WriteLine("What is your name?");
 
 Player? player = new(name: Console.ReadLine());
 
 Console.WriteLine($"Hello {player.GetName()}");
 
-while (true)
+while (!GameOver)
 {
     Console.WriteLine("What would you like to do?");
 
     var command = Console.ReadLine();
 
-    if(command.Contains("Inventory") && command.Contains("add"))
+    if(command.ToLower().Contains("inventory") && command.ToLower().Contains("add"))
     {
         Console.WriteLine("What would you like to add to inventory?");
         var nameOfItem = Console.ReadLine();
@@ -24,4 +26,10 @@ while (true)
         Console.WriteLine($"Item added to inventory. You currently have {player.GetInventoryCount()} items in your inventory, weighing a total of {player.GetInventoryWeight()}kg.");
     }
 
+    if(command.ToLower().Contains("go left"))
+    {
+        Console.WriteLine("You are eaten by a hungry Allosaurus! 🦖");
+        Console.WriteLine("GAME OVER");
+        GameOver = true;
+    }
 }
