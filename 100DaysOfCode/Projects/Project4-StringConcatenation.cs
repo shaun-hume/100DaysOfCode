7cc49ec28163c89651e1ce2c4862bbdd39a292db
Shaun Hume <s.d.hume88@gmail.com>
2022-01-14 23:32:41 +0800

    changed tests around for proper comparison

--- 100DaysOfCode/Projects/Project4-StringConcatenation.cs
+++ 100DaysOfCode/Projects/Project4-StringConcatenation.cs
@@ -9,39 +9,71 @@ namespace _100DaysOfCode.Projects
 		{
             Console.Clear();
             Typewrite($"Testing is beginning");
+            var runAmount = 0;
+                var stringBuilder = new StringBuilder();
 
-            RunAndReportStringConcatenationWithPlusOperator();
-			RunAndReportStringConcatenationWithStringBuilder();
-		}
-
-        private void RunAndReportStringConcatenationWithStringBuilder()
-        {
             var now = DateTime.Now;
-            var stringBuilder = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                stringBuilder.Clear();
+                RunAndReportStringConcatenationWithStringBuilder(stringBuilder);
+                runAmount = i;
+            }
+            var finishTime = DateTime.Now;
 
-            for (int i = 0; i < 100000; i++)
+            Typewrite($"\r\nString Builder: It took {finishTime - now} seconds and there were {runAmount} tests run.");
+
+
+            now = DateTime.Now;
+            for (int i = 0; i < 10000; i++)
+            {
+            RunAndReportStringConcatenationWithPlusOperator();
+                runAmount = i;
+            }
+            finishTime = DateTime.Now;
+
+            Typewrite($"\r\nPlus Operator: It took {finishTime - now} seconds and there were {runAmount} tests run.");
+
+            now = DateTime.Now;
+            for (int i = 0; i < 10000; i++)
+            {
+                stringBuilder.Clear();
+                RunAndReportStringConcatenationWithStringBuilder(stringBuilder);
+                runAmount = i;
+            }
+            finishTime = DateTime.Now;
+
+            Typewrite($"\r\nString Builder Test 2: It took {finishTime - now} seconds and there were {runAmount} tests run.");
+
+            now = DateTime.Now;
+            for (int i = 0; i < 10000; i++)
+            {
+                RunAndReportStringConcatenationWithPlusOperator();
+                runAmount = i;
+            }
+            finishTime = DateTime.Now;
+
+            Typewrite($"\r\nPlus Operator Test 2: It took {finishTime - now} seconds and there were {runAmount} tests run.");
+        }
+
+        private void RunAndReportStringConcatenationWithStringBuilder(StringBuilder stringBuilder)
+        {
+
+            for (int i = 0; i < 12; i++)
             {
                 stringBuilder.Append(i);
             }
 
-            var finishTime = DateTime.Now;
-
-            Typewrite($"\r\nString Builder: It took {finishTime - now} seconds");
         }
 
         private void RunAndReportStringConcatenationWithPlusOperator()
         {
-            var now = DateTime.Now;
             var text = "";
 
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 12; i++)
             {
                 text += i.ToString();
             }
-
-            var finishTime = DateTime.Now;
-
-            Typewrite($"\r\nPlus Operator: It took {finishTime - now} seconds");
         }
     }
 }
