a523d9eabb534e335beac75ea0a82f656a38b407
Shaun Hume <shaunhume@Shauns-MBP.localdomain>
2022-01-15 23:41:22 +0800

    add information and deploy script

---
+++ 100DaysOfCode/OtherProjects/100DaysAPI/100DaysAPI/deploy.sh
@@ -0,0 +1,7 @@
+echo "---------- Removing old 100Days API ----------"
+docker ps -a | awk '{ print $1,$2 }' | grep 100daysapi | awk '{print $1 }' | xargs -I {} docker stop rm {}
+
+echo "---------- Building 100days API ----------"
+docker build -t 100daysapi .
+echo "---------- Running 100days API ----------"
+docker run -d --rm -it -p 8001:80 100daysapi
