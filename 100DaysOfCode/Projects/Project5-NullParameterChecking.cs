5dc7b32e1ef0d3321df6a74c2defbb55446d7972
Shaun Hume <s.d.hume88@gmail.com>
2022-01-17 23:36:35 +0800

    Project 5 null param checking

---
+++ 100DaysOfCode/Projects/Project5-NullParameterChecking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace _100DaysOfCode.Projects
+{
+	public class Project5_NullParameterChecking
+	{
+		public Project5_NullParameterChecking()
+		{
+			List<string> list = null;
+
+			TriggerNullParameterException(list);
+		}
+
+        private void TriggerNullParameterException(List<string> testParameter!!)
+        {
+			return;
+        }
+    }
+}
+
