99b666948b0c75287a681bdee36a577f99c09d59
Shaun Hume <shaunhume@Shauns-MBP.localdomain>
2022-01-21 21:09:15 +0800

    update gitignore to ignore connection string file and update babydbcontext

--- 100DaysOfCode/OtherProjects/100DaysAPI/100DaysAPI/DbContexts/BabyDbContext.cs
+++ 100DaysOfCode/OtherProjects/100DaysAPI/100DaysAPI/DbContexts/BabyDbContext.cs
@@ -5,9 +5,11 @@ namespace _100DaysAPI.DbContexts
 {
 	public class BabyDbContext : DbContext
 	{
-		public BabyDbContext(DbContextOptions<BabyDbContext> options)
-		{	
-			var x = options;
-		}
+		public DbSet<MilkLog> MilkLogs { get; set; }
+		public DbSet<SleepLog> SleepLogs { get; set; }
+		public DbSet<ExerciseLog> ExerciseLogs { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseNpgsql("Host=my_host;Database=my_db;Username=my_user;Password=my_pw");
 	}
 }
