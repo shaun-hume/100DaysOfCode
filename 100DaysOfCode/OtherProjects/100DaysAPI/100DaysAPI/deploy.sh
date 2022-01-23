3a2c5f77664c525cf0a1fb1eee1d5acfe2d1e4d1
Shaun Hume <shaunhume@Shauns-MBP.localdomain>
2022-01-23 21:20:06 +0800

    continue to fix environment variables

--- 100DaysOfCode/OtherProjects/100DaysAPI/100DaysAPI/deploy.sh
+++ 100DaysOfCode/OtherProjects/100DaysAPI/100DaysAPI/deploy.sh
@@ -4,4 +4,4 @@ docker ps -a | awk '{ print $1,$2 }' | grep 100daysapi | awk '{print $1 }' | xar
 echo "---------- Building 100days API ----------"
 docker build -t 100daysapi .
 echo "---------- Running 100days API ----------"
-docker run -d --rm -it -p 5000:5000 100daysapi
+docker run --env-file environmentVariables.passwords -d --rm -it -p 5000:5000 100daysapi
