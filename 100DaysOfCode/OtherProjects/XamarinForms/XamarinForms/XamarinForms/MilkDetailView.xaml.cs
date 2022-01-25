f6751953930eaaf0cd543e093f17e710d44670f1
Shaun Hume <s.d.hume88@gmail.com>
2022-01-26 00:29:28 +0800

    added new detail view

--- 100DaysOfCode/OtherProjects/XamarinForms/XamarinForms/XamarinForms/MilkDetailView.xaml.cs
+++ 100DaysOfCode/OtherProjects/XamarinForms/XamarinForms/XamarinForms/MilkDetailView.xaml.cs
@@ -11,26 +11,65 @@ namespace XamarinForms
 {
     public partial class MilkDetailView : ContentPage
     {
-        public MilkLog milkLog;
+        private MilkLog originalMilkLog;
+        private MilkLog _newMilkLog = new MilkLog();
+        public MilkLog NewMilkLog {
+            get
+            {
+                return _newMilkLog;
+            }
+            set
+            {
+                _newMilkLog = value;
+            }
+        }
+
+        private void UpdateStatusOfSaveButton()
+        {
+            if (originalMilkLog == null) SaveChangesButton.IsEnabled = false;
+            else
+            {
+                if (originalMilkLog.ID == _newMilkLog.ID &&
+                    originalMilkLog.Type == _newMilkLog.Type &&
+                    originalMilkLog.Amount == _newMilkLog.Amount &&
+                    originalMilkLog.EstimatedAmount == _newMilkLog.EstimatedAmount &&
+                    originalMilkLog.MeasurementType == _newMilkLog.MeasurementType &&
+                    originalMilkLog.Comment == _newMilkLog.Comment &&
+                    originalMilkLog.StartTime == _newMilkLog.StartTime &&
+                    originalMilkLog.FinishTime == _newMilkLog.FinishTime)
+                {
+                    SaveChangesButton.IsEnabled = false;
+                }
+                else SaveChangesButton.IsEnabled = true;
+            }
+        }
+
+        public bool SaveChangesButtonIsEnabled { get; set; }
+
         public MilkDetailView(int id)
         {
             InitializeComponent();
+            BindingContext = this;
             UpdateMilkLog(id);
-
         }
 
         private void UpdateMilkLog(int id)
         {
             var client = new WebClient();
             var response = client.DownloadString(string.Concat("http://ubuntu:5000/BabyMonitor/GetMilk/", id));
-            milkLog = JsonConvert.DeserializeObject<MilkLog>(response);
+            originalMilkLog = JsonConvert.DeserializeObject<MilkLog>(response);
+            NewMilkLog = originalMilkLog;
 
-            EstimatedAmount.Text = milkLog.EstimatedAmount + milkLog.MeasurementType;
-            ActualAmount.Text = milkLog.Amount + milkLog.MeasurementType;
-            Comment.Text = milkLog.Comment;
-            StartTime.Text = milkLog.StartTime.ToShortTimeString();
-            FinishTime.Text = milkLog.FinishTime.ToShortTimeString();
+            //EstimatedAmount = originalMilkLog.EstimatedAmount;
+            //EstimatedAmountMeasurementType.Text = originalMilkLog.MeasurementType;
+            //ActualAmount.Text = originalMilkLog.Amount.ToString();
+            //ActualAmountMeasurementType.Text = originalMilkLog.MeasurementType;
+            //Comment.Text = originalMilkLog.Comment;
+            //StartDate.Date = originalMilkLog.StartTime;
+            //StartTime.Time = new TimeSpan(originalMilkLog.StartTime.Hour, originalMilkLog.StartTime.Minute, originalMilkLog.StartTime.Second);
+            //FinishDate.Date = originalMilkLog.FinishTime;
+            //FinishTime.Time = new TimeSpan(originalMilkLog.FinishTime.Hour, originalMilkLog.FinishTime.Minute, originalMilkLog.FinishTime.Second);
+
         }
     }
 }
-
