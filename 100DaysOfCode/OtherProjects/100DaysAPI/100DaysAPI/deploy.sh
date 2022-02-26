82124cdff11238f11b865f243a110379e3ab388b
Shaun Hume <shaunhume@Shauns-MBP.localdomain>
2022-02-26 11:26:53 +0800

    move BabyTime to separate repo

--- 100DaysOfCode/OtherProjects/100DaysAPI/100DaysAPI/deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "---------- Removing old 100Days API ----------"
-docker ps -a | awk '{ print $1,$2 }' | grep 100daysapi | awk '{print $1 }' | xargs -I {} docker stop rm {}
-
-echo "---------- Building 100days API ----------"
-docker build -t 100daysapi .
-echo "---------- Running 100days API ----------"
-docker run --env-file environmentVariables.passwords -d --restart always -it -p 5000:5000 100daysapi
